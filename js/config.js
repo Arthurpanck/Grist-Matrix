cb25f8f94eabbb1afd4b9ea990f9142a53ce3281
Arthurpanck <106170661+Arthurpanck@users.noreply.github.com>
2025-08-26 19:18:27 +0200

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,9 @@
+const CONFIG = {
+    MATRIX_HOMESERVER: 'https://discuter-mdl-core.territoirenumeriqueouvert.org',
+    MATRIX_ACCESS_TOKEN: '',
+    GRIST_TABLE_NAME: 'Notifications',
+    STORAGE_KEY: 'grist_matrix_notifications',
+    API_TIMEOUT: 30000,
+    SYNC_INTERVAL: 5000,
+    MATRIX_API_VERSION: 'v3',
+};
